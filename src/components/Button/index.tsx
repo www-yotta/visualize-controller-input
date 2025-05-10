6553d9919ad4c25da1bfcc506e36ce588e5ef814
www-yotta <84384463+www-yotta@users.noreply.github.com>
2025-05-10 06:42:47 +0000

    グラデーションを使わないように修正

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -32,22 +32,31 @@ export const Button = ({input,name,className}: Props) => {
 
   return (
     <div className={`w-24 h-24 border rounded-full flex items-center justify-center text-2xl overflow-hidden ${className}`}>
-      {name === "left" && left && <div className={`flex w-[100%] h-[100%] items-center justify-center text-[40px] bg-[gray] p-[4px]`}><Image alt="" src="left.png" className="w-[100%] h-[100%] object-contain" /></div>}
-      {name === "down" && down && <div className={`flex w-[100%] h-[100%] items-center justify-center text-[40px] bg-[gray] p-[4px]`}><Image alt="" src="down.png" className="w-[100%] h-[100%] object-contain" /></div>}
-      {name === "right" && right && <div className={`flex w-[100%] h-[100%] items-center justify-center text-[40px] bg-[gray] p-[4px]`}><Image alt="" src="right.png" className="w-[100%] h-[100%] object-contain" /></div>}
-      {name === "up" && up && <div className={`flex w-[100%] h-[100%] items-center justify-center text-[40px] bg-[gray] p-[4px]`}><Image alt="" src="up.png" className="w-[100%] h-[100%] object-contain" /></div>}
-      {name === "LT" && LT && <div className={`flex w-[100%] h-[100%] items-center justify-center text-[40px] bg-gradient-to-r from-yellow-300 from-50% to-red-500 to-50%`}></div>}
+      {name === "left" && left && <div className={`flex w-[100%] h-[100%] items-center justify-center text-[40px] bg-[gray] p-[4px]`}><Image width="96" height="96" unoptimized alt="" src="left.png" className="w-[100%] h-[100%] object-contain" /></div>}
+      {name === "down" && down && <div className={`flex w-[100%] h-[100%] items-center justify-center text-[40px] bg-[gray] p-[4px]`}><Image width="96" height="96" unoptimized alt="" src="down.png" className="w-[100%] h-[100%] object-contain" /></div>}
+      {name === "right" && right && <div className={`flex w-[100%] h-[100%] items-center justify-center text-[40px] bg-[gray] p-[4px]`}><Image width="96" height="96" unoptimized alt="" src="right.png" className="w-[100%] h-[100%] object-contain" /></div>}
+      {name === "up" && up && <div className={`flex w-[100%] h-[100%] items-center justify-center text-[40px] bg-[gray] p-[4px]`}><Image width="96" height="96" unoptimized alt="" src="up.png" className="w-[100%] h-[100%] object-contain" /></div>}
+      {name === "LT" && LT && <div className={`flex relative w-[100%] h-[100%] items-center justify-center text-[40px]`}>
+        <div className="absolute w-[50%] h-[100%] left-[0px] bg-yellow-300"></div>
+        <div className="absolute w-[50%] h-[100%] right-[0px] bg-red-500"></div>
+      </div>}
 
       
       {name === "buttonA" && buttonA && <div className={`flex w-[100%] h-[100%] items-center justify-center text-[40px] text-white bg-[gray]`}>Auto</div>}
       {name === "buttonB" && buttonB && <div className={`flex w-[100%] h-[100%] items-center justify-center text-[40px] text-white bg-[orange]`}>SP</div>}
-      {name === "RT" && RT && <div className={`flex w-[100%] h-[100%] items-center justify-center text-[40px] text-white bg-gradient-to-r from-amber-500 from-50% to-red-500 to-50%`}>SA</div>}
+      {name === "RT" && RT && <div className={`flex relative w-[100%] h-[100%] items-center justify-center text-[40px] text-white`}>
+        <span className="relative z-1">
+          SA
+        </span>
+        <div className="absolute w-[50%] h-[100%] left-[0px] bg-amber-500"></div>
+        <div className="absolute w-[50%] h-[100%] right-[0px] bg-red-500"></div>
+      </div>}
       {name === "L3" && L3 && <div className={`flex w-[100%] h-[100%] items-center justify-center text-[40px] text-white bg-[blue]`}>DI</div>}
 
       {name === "buttonX" && buttonX && <div className={`w-[100%] h-[100%] ${lowPClassName}`}></div>}
       {name === "buttonY" && buttonY && <div className={`w-[100%] h-[100%] ${middlePClassName}`}></div>}
       {name === "RB" && RB && <div className={`w-[100%] h-[100%] ${highPClassName}`}></div>}
-      {name === "LB" && LB && <div className={`flex w-[100%] h-[100%] items-center justify-center text-[40px] bg-[gray] pr-[4px]`}><Image alt="" src="throw.png" className="w-[100%] h-[100%] object-contain" /></div>}
+      {name === "LB" && LB && <div className={`flex w-[100%] h-[100%] items-center justify-center text-[40px] bg-[gray] pr-[4px]`}><Image width="96" height="96" unoptimized alt="" src="throw.png" className="w-[100%] h-[100%] object-contain" /></div>}
       {name === "R3" && R3 && <div className={`flex w-[100%] h-[100%] items-center justify-center text-[40px] text-white bg-[teal]`}>DP</div>}
     </div>
   );
