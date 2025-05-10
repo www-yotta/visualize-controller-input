e9e69e9425ab1b52f6adf04321e9ed6034d8b493
www-yotta <84384463+www-yotta@users.noreply.github.com>
2025-05-10 06:17:12 +0000

    ルール修正

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,9 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  rules: {
+    "@next/next/no-img-element": "off",
+  },
 });
 
 const eslintConfig = [
